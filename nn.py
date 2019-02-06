f1c0f4e6d09ae878d523c178532ce3531c8ddc70
hienhc <hienhc@nexlesoft.com>
2019-02-06 22:09:44 +0700

    feat: Add RNN cell

--- nn.py
+++ nn.py
@@ -264,3 +264,36 @@ class Embedding(Layer):
     
     def forward(self, input):
         return self.weight.index_select(input)
+
+
+class RNNCell(Layer):
+    def __init__(self, n_inputs, n_hidden, n_output, activation="sigmoid"):
+        super().__init__()
+        self.n_inputs = n_inputs
+        self.n_hidden = n_hidden
+        self.n_output = n_output
+
+        if activation == "sigmoid":
+            self.activation = Sigmoid()
+        elif activation == "tanh":
+            self.activation = Tanh()
+        else:
+            raise Exception("Unsupported non-linearity")
+        
+        self.w_ih = Linear(n_inputs, n_hidden)
+        self.w_hh = Linear(n_hidden, n_hidden)
+        self.w_ho = Linear(n_hidden, n_output)
+
+        self.parameters += self.w_ih.get_parameters()
+        self.parameters += self.w_hh.get_parameters()
+        self.parameters += self.w_ho.get_parameters()
+    
+    def forward(self, input, hidden):
+        prev_hidden = self.w_hh.forward(hidden)
+        combined = self.w_ih.forward(input) + prev_hidden
+        new_hidden = self.activation.forward(combined)
+        output = self.w_ho.forward(new_hidden)
+        return output, new_hidden
+    
+    def init_hidden(self, batch_size=1):
+        return Tensor(np.zeros((batch_size, self.n_hidden)), autograd=True)
