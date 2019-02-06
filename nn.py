bc54ba39aff1cd517c606b7e85a6813badd7e367
hienhc <hienhc@nexlesoft.com>
2019-02-06 09:34:19 +0700

    feat: Add backward

--- nn.py
+++ nn.py
@@ -1,11 +1,25 @@
 import numpy as np
 
 class Tensor(object):
-    def __init__(self, data):
+    def __init__(self, data, parents=None, op=None):
+        """
+        - parents: A list of Tensors used in creation of current Tensor
+        - op: The computation operator used in creation of current Tensor
+        """
         self.data = np.array(data)
+        self.op = op
+        self.parents = parents
+        self.grad = None
     
+    def backward(self, grad):
+        self.grad = grad
+
+        if self.op == 'add':
+            self.parents[0].backward(grad)
+            self.parents[1].backward(grad)
+
     def __add__(self, other):
-        return Tensor(self.data + other.data)
+        return Tensor(self.data + other.data, parents=[self, other], op="add")
     
     def __repr__(self):
         return str(self.data.__repr__())
